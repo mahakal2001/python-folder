f4bd2887d4e58a44bef7a7873eed9048113134af
Bhim Charan Bhakta <86588172+mahakal2001@users.noreply.github.com>
2021-12-24 14:33:47 +0530

    upload python file

---
+++ area_callculate.py
@@ -0,0 +1,15 @@
+#area of trangular
+'''
+base =float (input("enter the base:.."))
+hight =float (input("enter the hight:.."))
+
+trainguler= 0.5*base*hight
+
+print("area of traingular= ",trainguler)
+'''
+#area of circle:=
+radius =float (input("enter the radius:.."))
+
+circle= 3.1416*radius*radius
+
+print("area of circle= ",circle)
